f6ce4ee0286115e70475b5e56df3670e548fb4ef
Andrew <stroev.andrew@gmail.com>
2024-09-30 18:22:00 +0100

    NUKE all the unnecessary pipe viewing stuff

--- pew/pew.py
+++ pew/pew.py
@@ -1,7 +1,6 @@
 import importlib.metadata
 import subprocess
 import sys
-import textwrap
 
 from rich.console import Console
 from rich.panel import Panel
@@ -10,10 +9,9 @@ from rich.text import Text
 
 def main() -> None:
     console = Console(highlight=False, stderr=True)
-    max_width = max(console.width - 10, 16)
     pipe_data = None if sys.stdin.isatty() else sys.stdin.buffer.read()
 
-    if pipe_data is None and len(sys.argv) == 1:
+    if len(sys.argv) == 1:
         Console(stderr=False).print(Panel.fit(
             '🔫 [bold cyan]pew[/] [dim]-[/] the [yellow][bold]p[/]rogram [bold]e[/]xecution [bold]w[/]rapper[/]',
             subtitle='version ' + importlib.metadata.version(__package__),
@@ -23,34 +21,19 @@ def main() -> None:
 
     command_string = Text.from_markup('[cyan]pew:[/] ')
 
-    if len(sys.argv) > 1:
-        command_string.append(sys.argv[1], style='yellow')
-        for argument in sys.argv[2:]:
-            style = None
-            if argument.startswith('-'):
-                style = 'dim'
-            elif argument.isdigit():
-                style = 'orange'
-            elif any(ws in argument for ws in ' \r\n\t'):
-                style = 'green underline'
-            command_string.append(' ' + argument, style=style)
-            command_string.truncate(max_width, overflow='ellipsis')
-    else:
-        command_string.append_text(Text.from_markup('[dim]<[italic]no command[/]>'))
+    command_string.append(sys.argv[1], style='yellow')
+    for argument in sys.argv[2:]:
+        style = None
+        if argument.startswith('-'):
+            style = 'dim'
+        elif argument.isdigit():
+            style = 'orange'
+        elif any(ws in argument for ws in ' \r\n\t'):
+            style = 'green underline'
+        command_string.append(' ' + argument, style=style)
 
-    if pipe_data is not None:
-        try:
-            pipe_preview = textwrap.shorten(pipe_data.decode(), max_width)
-        except UnicodeDecodeError:
-            pipe_preview = '<[italic]binary[/]>'
-        command_string.append_text(Text.from_markup(f'\n[magenta] in[/][cyan]:[/] [dim]{pipe_preview}[/dim]'))
-
+    command_string.truncate(max(console.width - 10, 16), overflow='ellipsis')
     console.print(command_string)
 
-    if len(sys.argv) > 1:
-        if pipe_data is not None:
-            completed_process = subprocess.run(sys.argv[1:], input=pipe_data)
-        else:
-            completed_process = subprocess.run(sys.argv[1:])
-
-        exit(completed_process.returncode)
+    completed_process = subprocess.run(sys.argv[1:], input=pipe_data)
+    exit(completed_process.returncode)
